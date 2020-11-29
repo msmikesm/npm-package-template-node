4b880c6552ff758d9d1379f457281b7f9ce617ca
MichalS <m.siemienowicz.m@gmail.com>
2020-11-29 21:30:05 +0100

    remove prettier

--- src/__tests__/dumb.spec.ts
+++ src/__tests__/dumb.spec.ts
@@ -1,12 +1,11 @@
 import * as dumb from '../index';
 
 describe('Dumb test', () => {
-    test('1 + 2 should return numbers sum (3)', () => {
-        expect(dumb.dumbCalc(1, 2)).toStrictEqual(3);
-    });
+  test('1 + 2 should return numbers sum (3)', () => {
+    expect(dumb.dumbCalc(1, 2)).toStrictEqual(3);
+  });
 
-    test('NaN + NaN should return 0', () => {
-        expect(dumb.dumbCalc(NaN, NaN)).toStrictEqual(0);
-    });
-
+  test('NaN + NaN should return 0', () => {
+    expect(dumb.dumbCalc(NaN, NaN)).toStrictEqual(0);
+  });
 });
--- src/dumb/dumbCalc.ts
+++ src/dumb/dumbCalc.ts
@@ -1,5 +1,5 @@
 export const dumbCalc = (numA: number, numB: number): number => {
-    numA = numA || 0;
-    numB = numB || 0;
-    return numA + numB;
+  numA = numA || 0;
+  numB = numB || 0;
+  return numA + numB;
 };
